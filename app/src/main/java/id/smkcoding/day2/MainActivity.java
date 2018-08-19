a585d52916c4f535f5feb38c52fdcd08938166e3
Mochamad Lucky Pradana <luckypradana123@yandex.com>
2018-08-19 11:54:57 +0700

    add intent result

--- app/src/main/java/id/smkcoding/day2/MainActivity.java
+++ app/src/main/java/id/smkcoding/day2/MainActivity.java
@@ -1,6 +1,7 @@
 package id.smkcoding.day2;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,87 +11,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button mainButton;
+    private TextView mainText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // ambil referensi ID dari Layout XML
-        final TextView text1 = findViewById(R.id.text1);
-        Button buttonMinus = findViewById(R.id.buttonMinus);
-        Button buttonPlus = findViewById(R.id.buttonPlus);
+        mainButton = findViewById(R.id.mainButton);
+        mainText = findViewById(R.id.mainText);
 
-        // set onClick action ke buttonMinus
-        buttonMinus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String text1Text = text1.getText().toString();
-                int text1number = Integer.valueOf(text1Text);
-                text1number--;
-//                text1number = text1number - 1;
-//                text1number -= 1;
-                text1.setText(String.valueOf(text1number));
-            }
-        });
-
-        // set onClick action ke buttonPlus
-        buttonPlus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String text1Text = text1.getText().toString();
-                int text1number = Integer.valueOf(text1Text);
-
-                int newNumber = text1number + 1;
-                text1.setText(String.valueOf(newNumber));
-            }
-        });
-
-
-
-
-
-
-
-        Button buttonPindah = findViewById(R.id.buttonPindah);
-
-        buttonPindah.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                // IMPLICIT INTENT
-
-                Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-                // String, int, Object
-//                intent.putExtra("kode paket", "Laptop");
-                startActivity(intent);
-                finish();
-
-
-
-
-                // EXPLICIT INTENT
-//                Intent explicitIntent = new Intent(Intent.ACTION_DIAL);
-//                explicitIntent.setData(Uri.parse("tel:082234671637"));
-//                startActivity(explicitIntent);
-
-            }
-        });
-
-
-        final EditText editKata =  findViewById(R.id.editKata);
-        Button buttonKirim = findViewById(R.id.buttonKirim);
-
-        buttonKirim.setOnClickListener(new View.OnClickListener() {
+        mainButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(MainActivity.this, SecondActivity.class);
-
-                intent.putExtra("kata", editKata.getText().toString());
-
-                editKata.setText("");
-
-                startActivity(intent);
+                startActivityForResult(intent, 1);
             }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == 1 && resultCode == RESULT_OK){
+            if (data != null){
+                String dariSecond = data.getStringExtra("kata");
+                mainText.setText(dariSecond);
+            }
+        }
     }
 }
--- app/src/main/java/id/smkcoding/day2/SecondActivity.java
+++ app/src/main/java/id/smkcoding/day2/SecondActivity.java
@@ -1,20 +1,35 @@
 package id.smkcoding.day2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private EditText secondEdit;
+    private Button secondButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
 
-        TextView secondText = findViewById(R.id.secondText);
+        secondEdit = findViewById(R.id.secondEdit);
+        secondButton = findViewById(R.id.secondButton);
 
-        String text = getIntent().getStringExtra("kata");
+        secondButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("kata", secondEdit.getText().toString());
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
 
-        secondText.setText(text);
     }
 }
