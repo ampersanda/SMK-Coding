98a0602c9f709b33c2969b0a07a4a2b7a82be960
Mochamad Lucky Pradana <luckypradana123@yandex.com>
2018-08-19 12:02:04 +0700

    add explicit intent

--- app/src/main/java/id/smkcoding/day2/MainActivity.java
+++ app/src/main/java/id/smkcoding/day2/MainActivity.java
@@ -1,13 +1,35 @@
 package id.smkcoding.day2;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText mainEdit;
+    private Button mainButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mainEdit = findViewById(R.id.mainPhone);
+        mainButton = findViewById(R.id.mainButton);
+
+        mainButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String fromEditText = mainEdit.getText().toString();
+
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:"+fromEditText));
+                startActivity(intent);
+            }
+        });
     }
 }
