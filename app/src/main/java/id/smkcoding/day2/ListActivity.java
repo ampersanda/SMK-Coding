fa9d5fcf693474ff02b74d2ee363c5642c3d0b1d
Mochamad Lucky Pradana <luckypradana123@yandex.com>
2018-09-02 13:35:47 +0700

    add logout

--- app/src/main/java/id/smkcoding/day2/ListActivity.java
+++ app/src/main/java/id/smkcoding/day2/ListActivity.java
@@ -1,7 +1,9 @@
 package id.smkcoding.day2;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,7 +28,7 @@ public class ListActivity extends AppCompatActivity {
 
     private ListView listView;
     private EditText editText;
-    private Button button;
+    private Button button, signout;
     private List<String> listString;
     private FirebaseAuth auth;
 
@@ -47,6 +49,7 @@ public class ListActivity extends AppCompatActivity {
             listView = findViewById(R.id.listView);
             editText = findViewById(R.id.editText);
             button = findViewById(R.id.button);
+            signout = findViewById(R.id.signout);
 
             final ArrayAdapter arrayAdapter = new ArrayAdapter<>(ListActivity.this,
                     android.R.layout.simple_list_item_1, listString);
@@ -57,6 +60,29 @@ public class ListActivity extends AppCompatActivity {
             final DatabaseReference usersRef = database.getReference("users");
             final DatabaseReference uidRef = usersRef.child(uid);
             final DatabaseReference message = uidRef.child("message");
+
+            signout.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    new AlertDialog.Builder(ListActivity.this)
+                            .setCancelable(true)
+                            .setMessage("Are you sure to logout?")
+                            .setPositiveButton("No", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    dialogInterface.dismiss();
+                                }
+                            })
+                            .setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialogInterface, int i) {
+                                    auth.signOut();
+                                    startActivity(new Intent(ListActivity.this, MainActivity.class));
+                                    finish();
+                                }
+                            }).show();
+                }
+            });
 
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
